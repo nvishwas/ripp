9b6ef76a25233cc53b5299f4f9fc56ffc7685b8d
nvishwas <vishwasnn@gmail.com>
2015-11-28 19:16:28 +0530

    Update PseudoColorManager.java
    Read an image from fixed location add pseudo color and display input and output images

--- PseudoColorManager.java
+++ PseudoColorManager.java
@@ -1,5 +1,11 @@
+package rip;
 import javax.imageio.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -20,25 +26,61 @@ public class PseudoColorManager {
 	    System.out.println("Writers: " + 
 	        Arrays.asList(writeFormats));
 	    
-	    File InputFile = new File("/Users/lprasanna/Documents/XRays/Test.jpeg");
+	    File InputFile = new File("D:\\RIP\\project\\images\\Test.jpeg");
 	    System.out.println("File path : " + InputFile.getAbsolutePath());
 	    BufferedImage biImage = ImageIO.read(InputFile);
-	    int w = 1037;
-		int h =627;
-		int[] rgbArray = null;
-		int offset = 0;
-		int scansize = 100;
-		rgbArray = biImage.getRGB(0, 0, w, h, rgbArray, offset, scansize);
+	    Color c  = new Color(biImage.getRGB(313, 519));
+	    
+	    System.out.println("Image read: Number of pixels "+ biImage.getHeight()+ " " + biImage.getTileWidth() + "Image type is: " + biImage.getType()+"Pixel 313, 519: R" +c.getRed() + " G " + c.getGreen() + " B " + c.getBlue() );
+	    
+	    ImageIcon  icon = new ImageIcon(biImage);
+        JLabel jLabel = new JLabel("InputImage");
+        jLabel.setIcon(icon);
+       
+        JFrame editorFrame = new JFrame();
+		editorFrame.getContentPane().add(jLabel);
 
-		
-		int iloopend = rgbArray.length;
-		
-		for(int i=0; i <= iloopend -1; i++)
-		{
-			System.out.print( rgbArray[i])	;
-		}
-		
-
+        editorFrame.pack();
+        editorFrame.setLocationRelativeTo(null);
+        editorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        editorFrame.setVisible(true);
+        
+        BufferedImage clone = new BufferedImage(biImage.getWidth(),
+                biImage.getHeight(), biImage.getType());
+        Graphics2D g2d = clone.createGraphics();
+        g2d.drawImage(biImage, 0, 0, null);
+        g2d.dispose();
+        
+        int width = biImage.getWidth();
+        int height = biImage.getHeight();
+        int r, g, b;
+        double F, P;
+        F=0.9;
+        P=0.2;
+        Color nc;
+        
+        for (int i=0; i < width ; i++) {
+        	for (int j=0; j <  height; j++) {
+        		c  = new Color(biImage.getRGB(i, j));
+        		r = (int)(c.getRed() * 0.299);
+                g = (int)(c.getGreen() * 0.587);
+                b = (int)(c.getBlue() *0.114);       		
+        		nc = new Color(r,g,b);
+        		
+        		clone.setRGB(i, j, nc.getRGB());
+        	}
+        }
+        
+	    ImageIcon  oicon = new ImageIcon(clone);
+        JLabel ojLabel = new JLabel("Output Image");
+        ojLabel.setIcon(oicon);
+       
+        JFrame oeditorFrame =new JFrame();
+        oeditorFrame.getContentPane().add(ojLabel);
+        
+        oeditorFrame.pack();
+        oeditorFrame.setLocationRelativeTo(null);
+        oeditorFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        oeditorFrame.setVisible(true);      
 	}
-
 }
